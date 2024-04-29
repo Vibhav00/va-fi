0a2bf7c3d7a053f533073cace4129d60fd87b135
rahulkumar1805 <rahulfrenzi70@gmail.com>
2024-04-30 00:55:21 +0530

    splash screen

---
+++ app/src/main/java/com/example/va_fi/activities/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.va_fi.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.va_fi.MainActivity
+import com.example.va_fi.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val iHome = Intent(this@SplashActivity, MainActivity::class.java)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAGS_CHANGED)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(iHome)
+            finish()
+        },3000)
+    }
+}
